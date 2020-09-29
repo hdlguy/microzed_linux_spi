860b903e8779dede13d5879cd76efde295cbe006
pedro <pete@hdlguy.com>
2020-09-29 15:22:11 +0000

    added time of flight sensor to i2c_test

--- software/apps/i2c_test/test.c
+++ software/apps/i2c_test/test.c
@@ -9,14 +9,13 @@
 #include <sys/ioctl.h>
 #include <math.h>
 
-// program to read the TI HDC108 temperature and humidity sensor.
+// program to read the TI HDC1080 temperature and humidity sensor.
 int main(int argc,char** argv)
 {
-    printf("running i2c test.\n");
 
     int i2c_bus = 0;     // normally dynamically determined
-    int addr = 0x40;        // The I2C address
-    printf("bus = %d, addr = 0x%02x\n", i2c_bus, addr);
+    int hdc1080_addr = 0x40;   // I2C address
+    int isl29501_addr = 0x57;   // I2C address
 
     int file;
     char filename[20];
@@ -31,7 +30,9 @@ int main(int argc,char** argv)
     }
 
     // set the I2C address
-    if (ioctl(file, I2C_SLAVE, addr) < 0) {
+    printf("\naccessing HDC1080 temperature/humidity sensor.\n");
+    printf("bus = %d, addr = 0x%02x\n", i2c_bus, hdc1080_addr);
+    if (ioctl(file, I2C_SLAVE, hdc1080_addr) < 0) {
         printf("ioctl failed\n");
         exit(1);
     }
@@ -47,7 +48,7 @@ int main(int argc,char** argv)
     read(file, buf, 2);
     regval = ((buf[0] << 8) + buf[1]);
     floatval = 165.0*regval/pow(2.0,16.0) - 40.0;
-    printf("regval = 0x%04x, temperature = %lf\n", regval, floatval);
+    printf("HDC1080 regval = 0x%04x, temperature = %lf\n", regval, floatval);
 
     buf[0] = 0x01; // register address for humidity sensor
     write(file, buf, 1);
@@ -55,19 +56,42 @@ int main(int argc,char** argv)
     read(file, buf, 2);
     regval = ((buf[0] << 8) + buf[1]);
     floatval = 100.0*regval/pow(2.0,16.0);
-    printf("regval = 0x%04x, relative humidity = %lf\n", regval, floatval);
+    printf("HDC1080 regval = 0x%04x, relative humidity = %lf\n", regval, floatval);
 
     buf[0] = 0xfe; // register address for manufacturer ID
     write(file, buf, 1);
     read(file, buf, 2);
     regval = ((buf[0] << 8) + buf[1]);
-    printf("manufacturer id = 0x%04x\n", regval);
+    printf("HDC1080 manufacturer id = 0x%04x\n", regval);
 
     buf[0] = 0xff; // register address for device ID
     write(file, buf, 1);
     read(file, buf, 2);
     regval = ((buf[0] << 8) + buf[1]);
-    printf("device id = 0x%04x\n", regval);
+    printf("HDC1080 device id = 0x%04x\n", regval);
+
+    
+    // access the ISL29501 time of flight sensor
+    // set the I2C address
+    printf("\naccessing ISL29501 time of flight sensor.\n");
+    printf("bus = %d, addr = 0x%02x\n", i2c_bus, isl29501_addr);
+    if (ioctl(file, I2C_SLAVE, isl29501_addr) < 0) {
+        printf("ioctl failed\n");
+        exit(1);
+    }
+
+    buf[0] = 0x00; // register address for device ID
+    write(file, buf, 1);
+    read(file, buf, 1);
+    regval = buf[0];
+    printf("ISL29501 device id = 0x%04x\n", regval);
+
+    buf[0] = 0x13; // register address for sample control
+    write(file, buf, 1);
+    read(file, buf, 1);
+    regval = buf[0];
+    printf("ISL29501 sample control = 0x%04x\n", regval);
+
 
     return 0;
 }
