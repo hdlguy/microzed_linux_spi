fb2767b38b7df588f0b43dd37ed21feb52a9af72
pete <peter.dudley@precisionplanting.com>
2021-06-11 16:57:18 -0400

    tweaked board definition version

--- fpga/implement/setup.tcl
+++ fpga/implement/setup.tcl
@@ -4,7 +4,7 @@ file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
 #create_project -part xc7z020clg400-1 -force proj 
 create_project -force proj 
-set_property board_part em.avnet.com:microzed_7020:part0:1.1 [current_project]
+set_property board_part em.avnet.com:microzed_7020:part0:1.0 [current_project]
 set_property target_language verilog [current_project]
 set_property default_lib work [current_project]
 load_features ipintegrator
